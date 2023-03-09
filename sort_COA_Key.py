0ce499706a86789639469cda24703442a0d2ba40
2021sam <7050592@gmail.com>
2023-03-09 00:31:37 -0800

    Sort COA by EXPENSE & DESCRIPTION

--- sort_COA_Key.py
+++ sort_COA_Key.py
@@ -2,6 +2,6 @@ import pandas as pd
 file_coa = "Chart_Of_Accounts_Mappings.txt"
 coa = pd.read_csv(file_coa, encoding='latin1', thousands=',')
 print(coa)
-coa_sorted = coa.sort_values(by=['EXPENSE'])
+coa_sorted = coa.sort_values(by=['EXPENSE', 'DESCRIPTION'])
 print(coa_sorted)
 coa_sorted.to_csv('sorted_' + file_coa, index=False)
