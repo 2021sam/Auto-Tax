072ff90f2a7299699cad38f62c473ea365ba08a0
2021sam <7050592@gmail.com>
2023-03-10 01:56:22 -0800

    Cleaning

--- sax.py
+++ sax.py
@@ -1,10 +1,8 @@
-
 #   SAX - Sam Personal Tax Assistant
 #   Sam Portillo
 #   12/15/2019
-
+#   Revised 2023.03.10
 #   Version 1.1
-#   pip install pyinstaller
 # 
 from pathlib import Path
 
@@ -16,7 +14,6 @@ import pandas as pd         # pip install pandas
 import csv
 
 root = Tk()
-# Create the menubar
 menubar = Menu( root )
 root.config( menu=menubar)
 
@@ -25,21 +22,18 @@ def search_multiple_mappings(file_coa):
     #   Mapping expenses uses first match.
     #      This implies j can be in i but i can not be in j
     # COSTCO
-    # COSTCO GAS
+    # COSTCO GAS        # In this case, no write off for gas
 
     coa = pd.read_csv(file_coa, encoding='latin1', thousands=',')
-    # print( coa.iloc[1:] )
-    # print( coa.iloc[300:].iterrows() )
     match = False
     for i, row in coa.iterrows():
         for j, row in coa.iloc[i+1:].iterrows():
-            # if coa.loc[j, 'DESCRIPTION'] in coa.loc[i, 'DESCRIPTION']:
             if coa.loc[i, 'DESCRIPTION'] in coa.loc[j, 'DESCRIPTION']:
                 match = True
-                print( i+2, coa.loc[i, 'DESCRIPTION'], j+2, coa.loc[j, 'DESCRIPTION'] )     # Add 1 for headings + 1 for index to id
+                print(i+2, coa.loc[i, 'DESCRIPTION'], j+2, coa.loc[j, 'DESCRIPTION'] )     # Add 1 for headings + 1 for index to id
 
     if match:
-        print('Fail ! Multiple mappings found.')    
+        print('Fail ! Multiple mappings found.')
     if not match:
         print('Success !  No multiple matches.')
 
@@ -59,8 +53,6 @@ def get_checks():
         check_mappings = csv.reader(csvfile, delimiter=',', quotechar='|')
         checks = {}
         for row in check_mappings:
-            # print(', '.join(row))
-            # print(row)
             checks[ int(row[0]) ] = int(row[1])
     print(checks)
     return checks
@@ -69,42 +61,36 @@ def get_checks():
 def map(file_coa, transaction_file_name):
     chase = pd.read_csv( transaction_file_name, dtype={7: object}, skiprows = 1 )
     chase.columns.values
-    # print( chase.info() )     # Data structure
     cnames = ["Details","Posting Date","Description","Amount","Type","Balance","Check","KEY"]
     chase.columns = cnames
     chase['KEY'] = chase['KEY'].fillna(0)
-    # file_coa = "Chart_Of_Accounts_Mappings.txt"
     coa = pd.read_csv(file_coa, encoding='latin1', thousands=',')
-
     checks = get_checks()
     mia = 0
     for i, row in chase.iterrows():
         match = False
-
-
-    # for i, row in chase.iterrows():
         if chase.loc[i, 'Details'] == 'CHECK':
             check_number = int(chase.loc[i, 'Check'])
             print( check_number )
             chase.loc[i, 'KEY'] = checks[check_number]
-            continue
+            # continue
 
-
-        # if chase.loc[i, 'Details'] != 'CHECK':
-        for j, row in coa.iterrows():
-            if coa.loc[j, 'DESCRIPTION'] in chase.loc[i, 'Description']:
-                match = True
-                chase.loc[i, 'KEY'] = coa.loc[j, 'EXPENSE']
-                # print( i, chase.loc[i, 'KEY'], chase.loc[i, 'Description'], chase.loc[i, 'Amount'] )
-        if not match:
-            mia += 1
-            print( i, chase.loc[i, 'KEY'], chase.loc[i, 'Description'], chase.loc[i, 'Amount'] )
+        if chase.loc[i, 'Details'] != 'CHECK':
+            for j, row in coa.iterrows():
+                if coa.loc[j, 'DESCRIPTION'] in chase.loc[i, 'Description']:
+                    match = True
+                    chase.loc[i, 'KEY'] = coa.loc[j, 'EXPENSE']
+                    # print( i, chase.loc[i, 'KEY'], chase.loc[i, 'Description'], chase.loc[i, 'Amount'] )
+            if not match:
+                mia += 1
+                print( i, chase.loc[i, 'KEY'], chase.loc[i, 'Description'], chase.loc[i, 'Amount'] )
 
     print(f'MIA = {mia}')
     return mia, chase
 
 
 def list_checks(transaction_file_name):
+    print('list_checks ')
     chase = pd.read_csv( transaction_file_name, dtype={7: object}, skiprows = 1 )
     chase.columns.values
     cnames = ["Details","Posting Date","Description","Amount","Type","Balance","Check","KEY"]
@@ -174,7 +160,6 @@ subMenu.add_command(label="About Us", command=about_us)
 subMenu.add_command(label="Help Me", command=help)
 
 subMenu = Menu(menubar, tearoff=0)
-# menubar.add_cascade(label="Tax", command=join)
 subMenu.add_cascade(label="Tax", command=join)
 
 root.geometry('500x300')
