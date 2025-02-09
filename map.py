bd1dc4f3f252ae060d49dc5be58fef0d6a971c5d
2021sam <2021sam@Zyxes-Mac-mini.local>
2025-02-08 17:48:04 -0800

    restructured file locations

--- map.py
+++
@@ -1,9 +0,0 @@
-column_mapping = {
-    "Date": "Posting Date",       # Map Date to Posting Date
-    "Description": "Description", # Direct mapping
-    "Debit": "Amount",            # Debit becomes Amount
-    "Credit": "Amount",           # Credit becomes Amount (negative)
-}
-
-# print( column_mapping.items() )
-print(column_mapping['Date'])
